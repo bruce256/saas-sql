42a38bfeed970603d4e9a6c57b9c43ed1fc93176
“bruce256” <326375225@qq.com>
2023-10-15 10:05:32 +0800

    单元测试

--- src/test/java/com/bruce/jr/SqlProcessTest.java
+++ src/test/java/com/bruce/jr/SqlProcessTest.java
@@ -23,4 +23,25 @@ public class SqlProcessTest {
 		System.out.println(sql);
 	}
 	
+	@Test
+	public void star() {
+		SaasSqlInterceptor saasSqlInterceptor = new SaasSqlInterceptor();
+		String             sql                = saasSqlInterceptor.processSelectSql("select * from t ", "4000");
+		System.out.println(sql);
+	}
+	
+	@Test
+	public void groupBy() {
+		SaasSqlInterceptor saasSqlInterceptor = new SaasSqlInterceptor();
+		String             sql                = saasSqlInterceptor.processSelectSql("select a, b, c from t where a = '1' and b = 3 group by c", "4000");
+		System.out.println(sql);
+	}
+	
+	@Test
+	public void orderBy() {
+		SaasSqlInterceptor saasSqlInterceptor = new SaasSqlInterceptor();
+		String             sql                = saasSqlInterceptor.processSelectSql("select a, b, c from t where a = '1' and b = 3 order by c", "4000");
+		System.out.println(sql);
+	}
+	
 }
